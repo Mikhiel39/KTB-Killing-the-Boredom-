97102321ef3d77129c26538edb7055ae193bf588
Mikhiel <mikhielbenji9@gmail.com>
2023-05-09 11:59:54 +0530

    Changes in memes page in sharing the photos by link

--- Meme Generator/js/script.js
+++ Meme Generator/js/script.js
@@ -5,15 +5,20 @@ const textWrapper = document.querySelector(".text-wrapper");
 const closeBtnTemplate = document.querySelector(".close-button-template");
 const closeBtnText = document.querySelector(".close-button-text");
 const imageInput = document.querySelector(".image-input");
-const downloadBtn = document.querySelector(".download-btn");
 const shareBtn = document.querySelector(".share-btn");
+const shareOptions = document.querySelector(".share-options");
 const templateImgList = document.getElementsByClassName("memeTemplate");
 const topTextInput = document.querySelector(".top-text-input");
 const bottomTextInput = document.querySelector(".bottom-text-input");
 const memeFrame = document.querySelector(".output");
 const submitBtn = document.querySelector(".text-submit-btn");
+const memeBtn = document.querySelector(".meme-btn");
+const memeImg = document.querySelector(".meme-img");
+const nextBtn = document.querySelector(".next-btn");
+const inputColor = document.querySelector(".input-color");
+const inputFontSize = document.querySelector(".input-font");
 
-let image, imageDataURL;
+let image, imageDataURL, count = 0, link;
 
 let imageCreate = (url) => {
     image = new Image();
@@ -23,18 +28,24 @@ let imageCreate = (url) => {
     }, { once: true })
 }
 
+//array of templates
 let memeAssign = () => {
     for (const temp of templateImgList) {
         temp.onclick = () => imageCreate(temp.querySelector('img').src);
     }
 }
 
-memeAssign();
-
+let display = () => {
+    templateWrapper.style.display = 'none';
+    textWrapper.style.display = 'none';
+    memeImg.style.display = 'none';
+    memeFrame.style.display = 'block';
+    shareOptions.style.display = 'none';
+}
 
 templateBtn.onclick = () => {
-    if (textWrapper.style.display != 'None')
-        textWrapper.style.display = 'None';
+    display();
+    memeAssign();
     templateWrapper.style.display = templateWrapper.style.display == 'block' ? 'None' : 'block';
 }
 
@@ -44,14 +55,19 @@ closeBtnTemplate.onclick = () => {
 
 //If text button clicked
 textBtn.onclick = () => {
-    if (templateWrapper.style.display != 'None')
-        templateWrapper.style.display = 'None';
+    display();
     textWrapper.style.display = textWrapper.style.display == 'block' ? 'None' : 'block';
 }
 
 //if close text pallet button clicked
 closeBtnText.onclick = () => {
     textWrapper.style.display = textWrapper.style.display == 'block' ? 'None' : 'None';
+}
+
+shareBtn.onclick = () => {
+    display();
+    shareOptions.style.display = shareOptions.style.display == 'block' ? 'None' : 'block';
+    shareMeme();
 }
 
 // function resizeAndDownload(e) {
@@ -87,7 +103,10 @@ closeBtnText.onclick = () => {
 // }
 
 imageInput.addEventListener("change", (e) => {
+    memeImg.style.display = 'none';
+    memeFrame.style.display = 'block';
     imageCreate(URL.createObjectURL(e.target.files[0]));
+    count = 1;
 })
 
 submitBtn.onclick = () => {
@@ -105,7 +124,7 @@ function updateMeme(memeFrame, image, topTextInput, bottomTextInput) {
     const ctx = memeFrame.getContext("2d");
     const width = image.width;
     const height = image.height;
-    const fontSize = Math.floor(width / 12);
+    const fontSize = inputFontSize.value;
     const yOffset = height / 8;
 
     //update image background 
@@ -116,7 +135,7 @@ function updateMeme(memeFrame, image, topTextInput, bottomTextInput) {
     //prepare text
     ctx.strokestyle = 'black';
     ctx.lineWidth = Math.floor(fontSize / 4);
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = inputColor.value;
     ctx.textAlign = 'center';
     ctx.lineJoin = 'round';
     ctx.font = `${fontSize}px sans-serif`;
@@ -131,4 +150,43 @@ function updateMeme(memeFrame, image, topTextInput, bottomTextInput) {
     ctx.strokeText(bottomTextInput, width / 2, height - yOffset);
     ctx.fillText(bottomTextInput, width / 2, height - yOffset);
     document.querySelector(".img-download").href = memeFrame.toDataURL();
+    document.querySelector(".copy-content").innerHTML = encodeURIComponent(memeFrame.toDataURL());
+    document.querySelector(".copy-btn").onclick = () => {
+        navigator.clipboard.writeText(document.querySelector(".copy-content").innerHTML);
+    }
+    if(imageInput.files[0] == undefined) document.querySelector(".img-download").download = image.src.substring(28);
+    else document.querySelector(".img-download").download = imageInput.files[0].name;
 }
+
+function shareMeme() {
+    link = image.src;
+    let title = encodeURIComponent("Hi everyone, please check this out : \n");
+
+    document.querySelector(".facebook-btn").href = `https://www.facebook.com/sharer.php?u=${link}`;
+    document.querySelector(".whatsapp-btn").href = `https://web.whatsapp.com/send?text=${title}${link}`;
+    document.querySelector(".pinterest-btn").href = `https://pinterest.com/pin/create/bookmarklet/?url=${link}&description=${title}`;
+    document.querySelector(".twitter-btn").href = `https://twitter.com/share?url=${link}&text=${title}`;
+    document.querySelector(".linkedin-btn").href = `https://www.linkedin.com/post/new/?url=${link}&title=${title}`;
+}
+
+const updateDetails = (url, title) => {
+    document.querySelector(".meme-img img").src = url;
+    document.querySelector(".meme-title").innerHTML = title;
+}
+
+const showMemes = () => {
+    memeFrame.style.display = 'none';
+    memeImg.style.display = 'block';
+    templateWrapper.style.display = 'none';
+    textWrapper.style.display = 'none';
+    fetch("https://meme-api.com/gimme/wholesomememes")
+        .then((Response) => Response.json())
+        .then((data) => {
+            updateDetails(data.url, data.title);
+            document.querySelector(".img-download").href = data.url;
+            document.querySelector(".img-download").download = data.title;
+        });
+}
+
+memeBtn.addEventListener("click",showMemes);
+nextBtn.addEventListener("click",showMemes);
